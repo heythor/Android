353d129473db5d513de7240d4142cccdefcd40ab
Heitor Peixoto <heitor.fabricio@hotmail.com>
2022-11-22 10:21:06 -0300

    add project Login

---
+++ Login/app/src/main/java/com/projetos/txoza/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.projetos.txoza
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
